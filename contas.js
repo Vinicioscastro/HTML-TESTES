f6abdfe0247d747c7328ba1d687c0ab06570f0fe
vinicioscastro <viniciospcastro19@gmail.com>
2022-02-13 16:22:54 -0300

    svg e contas

---
+++ contas.js
@@ -0,0 +1,15 @@
+aluguel = 200;
+net = 150;
+cartaoR = 150;
+cartao = 912;
+luz = 65 / 2;
+agua = 39 / 2;
+
+total = aluguel + net + cartao + cartaoR + luz + agua;
+emMaos = 22;
+(tenho = 1418), 76;
+
+saldo = tenho + emMaos - total;
+console.log(`total ${total}`);
+
+console.log(`Saldo: ${saldo}`);
