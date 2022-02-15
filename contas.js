ec260d21f0f56972530236ae5d98e625b9f0c743
vinicioscastro <viniciospcastro19@gmail.com>
2022-02-15 14:29:58 -0300

    pagamento

--- contas.js
+++ contas.js
@@ -1,15 +1,14 @@
-aluguel = 200;
-net = 150;
-cartaoR = 150;
-cartao = 912;
-luz = 65 / 2;
-agua = 39 / 2;
+aluguel = 400; //pago
+net = 150; // pago cx, 
+cartaoR = 150; //pago
+cartao = 912; // pago
+luz = 65; // pago
+agua = 39; //pago
 
 total = aluguel + net + cartao + cartaoR + luz + agua;
-emMaos = 22;
-(tenho = 1418), 76;
+tenho = 1774.46;
 
-saldo = tenho + emMaos - total;
+saldo = tenho - total;
 console.log(`total ${total}`);
 
 console.log(`Saldo: ${saldo}`);
